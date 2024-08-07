177d1eb9eaff2c18ed92fcb11039484a1ef359b0
Alex Poputa <poputa.alex@gmail.com>
2024-08-07 21:17:06 +0300

    changed selectors

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,8 +1,8 @@
 import _ from "lodash";
 
 const STORAGE_KEY = "feedback-form-state";
-const email = document.getElementsByName('email');
-const message = document.getElementsByName('message');
+const email = document.querySelector('.feedback-form input');
+const message = document.querySelector('.feedback-form textarea');
 const submit = document.querySelector('.feedback-form button');
 
 class Data{
@@ -13,12 +13,8 @@ class Data{
 }
 
 
-const data = new Data(email[0].value, message[0].value)
-
-
-
 submit.addEventListener('click',_.throttle((e)=>{
     e.preventDefault();
-    
+    const data = new Data(email.value, message.value)
     console.log(data);
 },500))

47f1ffc9a74be8607d4e73938bde94bd3ffe93a4
Alex Poputa <poputa.alex@gmail.com>
2024-08-07 21:10:02 +0300

    started Task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,24 @@
+import _ from "lodash";
 
+const STORAGE_KEY = "feedback-form-state";
+const email = document.getElementsByName('email');
+const message = document.getElementsByName('message');
+const submit = document.querySelector('.feedback-form button');
+
+class Data{
+    constructor(user, setMessage){
+        this.user = user,
+        this.setMessage = setMessage
+    }
+}
+
+
+const data = new Data(email[0].value, message[0].value)
+
+
+
+submit.addEventListener('click',_.throttle((e)=>{
+    e.preventDefault();
+    
+    console.log(data);
+},500))

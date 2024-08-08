3883c7b279d414bd0b4449f2e8c6f2d0b8aab3e0
unknown <poputa.alex@gmail.com>
2024-08-08 15:17:45 +0300

    refactored code on task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,28 +5,24 @@ import _ from "lodash";
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
+const setTime = ()=>{
+    player.getCurrentTime().then((seconds)=> {
+        localStorage.setItem('time',seconds)
+    }).catch((error)=> {
+        console.error(error.message)
+    });
+}
+
 
 // getting timer from local storage
 player.on('play',()=> {
-   const seconds = localStorage.getItem('timer');
+   const seconds = localStorage.getItem('time');
    player.setCurrentTime(seconds)
 });
 
 // setting timer to local storage
-player.on('pause',()=> {
-    player.getCurrentTime().then((seconds)=> {
-        localStorage.setItem('timer',seconds)
-    }).catch((error)=> {
-        console.error(error.message)
-    });
-});
-player.on('seeking',()=>{
-    player.getCurrentTime().then((seconds)=> {
-       localStorage.setItem('timer',seconds)
-    })
-});
-player.on('timeupdate',_.throttle(()=>{
-    player.getCurrentTime().then((seconds)=> {
-       localStorage.setItem('timer',seconds);
-    })
-},1500));
+player.on('pause',setTime);
+
+player.on('seeking',setTime);
+
+player.on('timeupdate',_.throttle(setTime,1500));

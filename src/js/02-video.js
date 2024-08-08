a6ae08eb07abaee3beaea2f211124efeda669153
unknown <poputa.alex@gmail.com>
2024-08-08 15:11:57 +0300

    finished task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,32 @@
-import {Vimeo} from '@vimeo/player';
+import  Player  from "@vimeo/player";
+import _ from "lodash";
+
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo(iframe);
+const player = new Player(iframe);
 
-console.log(player);
+
+// getting timer from local storage
+player.on('play',()=> {
+   const seconds = localStorage.getItem('timer');
+   player.setCurrentTime(seconds)
+});
+
+// setting timer to local storage
+player.on('pause',()=> {
+    player.getCurrentTime().then((seconds)=> {
+        localStorage.setItem('timer',seconds)
+    }).catch((error)=> {
+        console.error(error.message)
+    });
+});
+player.on('seeking',()=>{
+    player.getCurrentTime().then((seconds)=> {
+       localStorage.setItem('timer',seconds)
+    })
+});
+player.on('timeupdate',_.throttle(()=>{
+    player.getCurrentTime().then((seconds)=> {
+       localStorage.setItem('timer',seconds);
+    })
+},1500));
